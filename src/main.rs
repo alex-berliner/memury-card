5f954aea1a18306ea3aaa2d8b78965a1e9b7e216
Your Name <you@example.com>
2021-11-13 21:12:54 -0800

    auto detect files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::num::ParseIntError;
+use walkdir::WalkDir;
 use notify::{DebouncedEvent, Watcher, RecursiveMode, watcher};
 use std::sync::mpsc::channel;
 use std::time::{Duration, SystemTime};
@@ -42,7 +44,6 @@ fn watch_files() {
     watcher.watch("/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).ss1", RecursiveMode::Recursive).unwrap();
     // watcher.watch("/home/alex/Mario & Luigi - Superstar Saga (USA, Australia).sav", RecursiveMode::Recursive).unwrap();
     // watcher.watch("/home/alex/Code/savesync/testfiles/old.txt", RecursiveMode::Recursive).unwrap();
-    // let mut hasher = Blake2b::new();
 
     loop {
         match rx.recv() {
@@ -88,7 +89,6 @@ fn do_links() {
 
 // take a directory. if "sync", ensure all files are up to date between the two folders. if "symlink", copy the original
 // file to the sync folder and replace the original with a symlink (optimization, start with copy mode only?).
-
 fn get_metadata() {
     // file scanner: look for files of specified file types in targeted directories. if found, add by copy to versioned
     // storage area. when adding to versioned storage area, use name to detect duplicates. if possible, use save header
@@ -108,8 +108,56 @@ fn get_metadata() {
     println!("{:?}", metadata.file_type());
 }
 
+fn find_savs() {
+    let walkdir = "/home/alex/Dropbox/sync";
+    let (tx, rx) = channel();
+    let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
+    for entry in WalkDir::new(walkdir) .follow_links(true) .into_iter() .filter_map(|e| e.ok()) {
+        let f_name = entry.file_name().to_string_lossy();
+        let sec = entry.metadata().unwrap().modified().unwrap();
+
+        // right now having two files registered copied on top of each other will ping pong back and forth which is why
+        // it's important to set up hashing to ensure that files that are currently being tracked aren't inserted into
+        // the system as current
+        if f_name.ends_with(".ss1") /* || f_name.ends_with(".ss2") || f_name.ends_with(".ss3") */ {
+            println!("{}", entry.path().to_str().unwrap());
+            watcher.watch(entry.path().to_str().unwrap(), RecursiveMode::Recursive).unwrap();
+        }
+    }
+
+    loop {
+        match rx.recv() {
+            Ok(event) =>
+                match event {
+                    DebouncedEvent::Write(p) | DebouncedEvent::Chmod(p) => {
+                        println!("Update: {:?}", p);
+                        let incage =  std::fs::metadata(&p).unwrap().modified().unwrap();
+                        let compage = std::fs::metadata("/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).ss2")
+                            .unwrap().modified().unwrap();
+                        let p_str = p.clone().into_os_string().into_string().unwrap();
+                        if let Ok(mut file) = fs::File::open(&p_str) {
+                            process::<Sha256, _>(&mut file, &p_str);
+                        }
+                        if incage > compage {
+                            std::fs::copy(&p, "/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).ss3");
+                            println!("update {:?}", p);
+                        }
+                    }
+                    DebouncedEvent::NoticeWrite(p) => println!("NoticeWrite {:?}", p),
+                    DebouncedEvent::Create(p) => println!("Create {:?}", p),
+                    DebouncedEvent::Remove(p) => println!("Remove {:?}", p),
+                    DebouncedEvent::NoticeRemove(p)  => println!("NoticeRemove {:?}", p),
+                    DebouncedEvent::Rename(a, b) => println!("Rename {:?} -> {:?}", a, b),
+                    _ => (),
+                },
+           Err(e) => println!("watch error: {:?}", e),
+        };
+    }
+}
+
 fn main() {
     // do_links();
-    watch_files();
+    // watch_files();
     // get_metadata();
+    find_savs();
 }
