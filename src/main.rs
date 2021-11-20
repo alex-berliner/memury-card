0bc37682178fd092fb8d1fa38696ec8c79c4a806
Alex Berliner <alexberliner@gmail.com>
2021-11-20 00:26:24 -0800

    hashing all files to hashset

--- src/main.rs
+++ src/main.rs
@@ -15,10 +15,6 @@ use sha2::{Digest, Sha256};
 use std::fs;
 use std::collections::{HashMap, HashSet};
 
-struct Savefile {
-    result: String,
-}
-
 fn do_links() {
     let mario_tuple = vec![
         ("/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).gba",
@@ -55,22 +51,19 @@ fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
 }
 
-fn file_sha256(path: &str) -> Savefile {
+fn file_sha256(path: &str) -> String {
+    // TODO: check file exists
     let bytes = std::fs::read(path).unwrap();
     let mut hasher = Sha256::new();
     hasher.update(bytes);
-    let s = Savefile {
-        // TODO: this produces a string like "[A3, FE, ..., E3]", but hey it's accurate. if this thing works i'll fix it
-        result: format!("{:02X?}", hasher.finalize()),
-    };
-    s
+    format!("{:02X?}", hasher.finalize())
 }
 
 fn find_savs() {
     let walkdir = "/home/alex/Dropbox/sync";
     let (tx, rx) = channel();
     let mut watcher = watcher(tx, Duration::from_secs(1)).unwrap();
-    let mut save_map/* : HashMap<&str, HashSet<&str>> */ = HashMap::new();
+    let mut save_map: HashMap<String, HashSet<String>> = HashMap::new();
 
     for entry in WalkDir::new(walkdir) .follow_links(true) .into_iter() .filter_map(|e| e.ok()) {
         let f_name = entry.file_name().to_string_lossy();
@@ -80,10 +73,17 @@ fn find_savs() {
         // it's important to set up hashing to ensure that files that are currently being tracked aren't inserted into
         // the system as current
         if f_name.ends_with(".ss1") /* || f_name.ends_with(".ss2") || f_name.ends_with(".ss3") */ {
-            println!("{}", entry.path().to_str().unwrap());
-            watcher.watch(entry.path().to_str().unwrap(), RecursiveMode::Recursive).unwrap();
+            let entry = entry.path().to_str().unwrap();
+            watcher.watch(entry, RecursiveMode::Recursive).unwrap();
+            let res = file_sha256(&entry);
+            println!("{:?}", entry);
+            println!("{:?}", res);
+            let set = save_map.entry(res).or_insert_with(HashSet::new);
+            set.insert(entry.to_string());
         }
     }
+    let hs = save_map.get(&file_sha256("/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).ss1")).unwrap();
+    println!("{:?}", hs.len());
 
     loop {
         match rx.recv() {
@@ -91,25 +91,10 @@ fn find_savs() {
                 match event {
                     DebouncedEvent::Write(p) | DebouncedEvent::Chmod(p) => {
                         println!("Update: {:?}", p);
-                        let incage =  std::fs::metadata(&p).unwrap().modified().unwrap();
-                        let compage = std::fs::metadata("/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).ss2")
-                            .unwrap().modified().unwrap();
-                        let p_str = p.clone().into_os_string().into_string().unwrap();
-                        if let Ok(mut file) = fs::File::open(&p_str) {
-                            let res = file_sha256(&p_str);
-                            // println!("{:?}", res.result);
-                            // https://stackoverflow.com/a/65550108
-                            match save_map.get(&res.result) {
-                                Some(v) => ()/* {
-                                    println!("yes existy {:?}", v);
-                                } */,
-                                None => {
-                                    let myset: HashSet<&str> = HashSet::new();
-                                    save_map.insert(&res.result, myset);
-                                    println!("insert {:?}", p_str);
-                                },
-                            }
-                        }
+                        // let p_str = p.clone().into_os_string().into_string().unwrap();
+                        // let incage =  std::fs::metadata(&p).unwrap().modified().unwrap();
+                        // let compage = std::fs::metadata("/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).ss2")
+                        //     .unwrap().modified().unwrap();
                         // if incage > compage {
                         //     std::fs::copy(&p, "/home/alex/Dropbox/sync/Mario & Luigi - Superstar Saga (USA, Australia).ss3");
                         //     println!("update {:?}", p);
