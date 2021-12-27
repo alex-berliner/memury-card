2eb70f2085cedba285b8307eef116352bb0d0245
Alex Berliner <alexberliner@gmail.com>
2021-12-27 15:01:53 -0500

    remove duplicate file/dir watchers

--- src/main.rs
+++ src/main.rs
@@ -63,8 +63,6 @@ struct Savedata {
 }
 
 enum HashmapCmd {
-    WatchDir(SaveDir),
-    WatchFile(SaveFile),
     Watch(SaveDef),
     Unwatch(String),
     Copy(PathBuf),
@@ -105,7 +103,7 @@ struct SaveDef {
     options: SaveOpts,
 }
 
-fn parse_save_json(json_file: &str, dir_accu: &mut Vec<SaveDir>, file_accu: &mut Vec<SaveFile>, save_accu: &mut Vec<SaveDef>) {
+fn parse_save_json(json_file: &str, save_accu: &mut Vec<SaveDef>) {
     let bytes = std::fs::read_to_string(json_file).unwrap();
     let json: Value = serde_json::from_str(&bytes).unwrap();
     let saves = json["saves"].as_array().unwrap();
@@ -138,29 +136,6 @@ fn parse_save_json(json_file: &str, dir_accu: &mut Vec<SaveDir>, file_accu: &mut
             options: saveopt,
         };
         save_accu.push(savedef);
-        //     let path = if saves[i]["sync_loc"] != Value::Null
-
-        //     let mut save = SaveDir {
-        //         dir: PathBuf::from(helper::strip_quotes(saves[i]["dir"].as_str().unwrap())),
-        //         filetypes: vec![],
-        //     };
-        //     let filetypes = saves[i]["filetypes"].as_array().unwrap();
-        //     for j in 0..filetypes.len() {
-        //         save.filetypes
-        //             .push(filetypes[j].as_str().unwrap().to_string());
-        //     }
-        //     dir_accu.push(save);
-        // } else if saves[i]["file"] != Value::Null {
-        //     let save = SaveFile {
-        //         file: PathBuf::from(helper::strip_quotes(saves[i]["file"].as_str().unwrap())),
-        //         sync_loc: if saves[i]["sync_loc"] != Value::Null {
-        //             PathBuf::from(helper::strip_quotes(saves[i]["sync_loc"].as_str().unwrap()))
-        //         } else {
-        //             PathBuf::from("")
-        //         },
-        //     };
-        //     file_accu.push(save);
-        // }
     }
 }
 
@@ -210,15 +185,7 @@ fn save_watcher(
     loop {
         match file_add_rx.recv().unwrap() {
             HashmapCmd::Watch(save) => {
-                println!("save");
                 save.print();
-            }
-            HashmapCmd::WatchFile(savefile) => {
-                watcher.watch(&savefile.file, RecursiveMode::NonRecursive);
-            }
-            HashmapCmd::WatchDir(saveloc) => {
-                // this is what makes events bubble up for file modification
-                watcher.watch(&saveloc.dir, RecursiveMode::Recursive);
             }
             HashmapCmd::Unwatch(rmpath) => {
                 // watcher.unwatch(&entry).unwrap();
@@ -237,10 +204,8 @@ fn save_watcher(
 }
 
 // find all files in @json_dir that end in .json, return a vector of SaveDir's from them
-fn get_save_descriptors(json_dir: &str) -> (Vec<SaveDir>, Vec<SaveFile>, Vec<SaveDef>) {
+fn get_save_descriptors(json_dir: &str) -> Vec<SaveDef> {
     let json_dir = helper::strip_quotes(json_dir);
-    let mut dir_accu: Vec<SaveDir> = vec![];
-    let mut file_accu: Vec<SaveFile> = vec![];
     let mut save_accu: Vec<SaveDef> = vec![];
 
     for entry in WalkDir::new(json_dir)
@@ -252,11 +217,11 @@ fn get_save_descriptors(json_dir: &str) -> (Vec<SaveDir>, Vec<SaveFile>, Vec<Sav
 
         if f_name.ends_with(".json") {
             let entry = entry.path().to_str().unwrap();
-            parse_save_json(&entry, &mut dir_accu, &mut file_accu, &mut save_accu);
+            parse_save_json(&entry, &mut save_accu);
             // file_add_tx.send(entry.clone().to_string());
         }
     }
-    (dir_accu, file_accu, save_accu)
+    save_accu
 }
 
 fn get_save_watch_entries(savelocs: &Vec<SaveDir>) -> Vec<String> {
@@ -284,20 +249,9 @@ fn get_save_watch_entries(savelocs: &Vec<SaveDir>) -> Vec<String> {
 
 // crawl through saves listed from save files and send results to watcher thread
 fn find_saves(json_dir: &str, file_add_tx: &mpsc::Sender<HashmapCmd>) {
-    let (savelocs, savefiles, saves) = get_save_descriptors(json_dir);
-    // let save_watch_entries = get_save_watch_entries(&savelocs);
-    for e in savelocs {
-        // println!("{}", e.dir);
-        file_add_tx.send(HashmapCmd::WatchDir(e));
-    }
-
-    for e in savefiles {
-        // println!("{}", e.dir);
-        file_add_tx.send(HashmapCmd::WatchFile(e));
-    }
+    let saves = get_save_descriptors(json_dir);
 
     for e in saves {
-        // println!("{}", e.dir);
         file_add_tx.send(HashmapCmd::Watch(e));
     }
 }
