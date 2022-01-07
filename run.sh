bfa0c3a5de46672b6fcd71d9c7786f39696e5d1a
Alex Berliner <alexberliner@gmail.com>
2022-01-07 09:13:08 -0500

    conditional run in build script

--- run.sh
+++ run.sh
@@ -4,4 +4,7 @@ cd assets
 RUST_LOG=debug RUST_BACKTRACE=1 cargo build
 rm -rf ./memurycard.exe
 cp ../target/debug/memurycard.exe .
-./memurycard.exe
+
+if [[ $1 == "run" ]]; then
+    ./memurycard.exe
+fi
